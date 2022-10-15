1df331515e3e9a3c36fbde451d87fa8cf8b168d1
Tiollit <Tiollit7@gmail.com>
2022-10-15 08:47:10 -0700

    Lab 1 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <windows.h>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
+	string a, c;
+	int b, v;
+	cout << "Incert symbol, speed of painting in miliseconds, line lenght and line type: H for horizontal, V for vertical " << endl;
+	cin >> a >> v >> b >> c;
+	for (int i = 0; i < b; i++)
+	{
+		if (c == "H")
+		{
+			Sleep(v); cout << a << " ";
+		}
+
+		else if (c == "V")
+		{
+			Sleep(v); cout << a << endl;
+		}
+	}
 
 	return 0;
 }
