1a7186e6b9672cbca9da6d9986d844aae3596959
Tiollit <Tiollit7@gmail.com>
2022-10-15 12:51:04 -0700

    Lab 2 alm done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
+	srand(time(NULL));
 	int b, c, sump = 0, sumk = 0;
 	string s;
 	cout << "Let's play dice game: " << endl;
@@ -16,11 +20,10 @@ int main()
 		{
 			cout << "Make your turn: " << endl;
 			system("pause");
-			b = rand() % 6;
-			c = rand() % 6;
+			b = rand() % 5 + 1;
+			c = rand() % 5 + 1;
 			sump += b;
-			sump += c;
-			s = "n";
+			sump += c;			
 		}
 		else if (s == "n")
 		{
@@ -29,8 +32,7 @@ int main()
 			b = rand() % 6;
 			c = rand() % 6;
 			sumk += b;
-			sumk += c;
-			s = "y";
+			sumk += c;			
 		}
 		if (b == 1)
 		{
@@ -127,14 +129,17 @@ int main()
 			cout << char(179) << " " << char(174) << " " << char(174) << " " << char(179) << endl;
 			cout << char(179) << " " << char(174) << " " << char(174) << " " << char(179) << endl;
 			cout << char(192) << char(196) << char(196) << char(196) << char(196) << char(196) << char(217) << endl;
-		}		
+		}	
+		if (s == "y") s = "n";
+		else s = "y";
 	}
 	cout << "End of the game - let's see total points: " << endl;
 	system("pause");
 	cout << "You total points: " << sump << endl;
 	cout << "Computer total points: " << sumk << endl;
 	if (sump > sumk) cout << "You Win! Congratulations" << endl;
-	else cout << "Sorry. You lose the game" << endl;
+	else if (sump < sumk) cout << "Sorry. You lose the game" << endl;
+	else cout << "This is draw. Nobody wins. Play another game." << endl;
 
 	return 0;
 }
