d50d00c68d7c642b9ee505f99d021eaf57d51fa3
Tiollit <Tiollit7@gmail.com>
2022-10-15 14:55:35 -0700

    Lab 5 beg

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <time.h>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
+  	int newar[100];
+	for (int i = 0; i < 100; i++)
+	{
+		newar[i] = rand() % 1000000 - 500000;
 
+	}
 	return 0;
 }
